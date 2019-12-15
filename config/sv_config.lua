591b005bc7427f167c445f88f1c9fc5d75461801
Wasied <wasied.rmid@gmail.com>
2019-12-15 20:34:02 +0100

    Adding admin system

--- config/sv_config.lua
+++ config/sv_config.lua
@@ -7,3 +7,6 @@ RealisticBase.Port = "3306" -- Port
 RealisticBase.User = "root" -- User
 RealisticBase.Pass = "" -- Password
 RealisticBase.Name = "realistic_base" -- Database name
+
+-- SteamID64 of the server owner (https://steamidfinder.com/lookup/)
+RealisticBase.SteamIDOwner = "76561198161920297"
